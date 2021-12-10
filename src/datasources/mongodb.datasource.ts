7b61375f367f926aeb24d8d63897744c0875c992
Ricardo PELAEZ-BOTERO <mahamantradas@hotmail.com>
2021-12-10 22:08:11 +0100

    Se retira el link de MongoDB

--- src/datasources/mongodb.datasource.ts
+++ src/datasources/mongodb.datasource.ts
@@ -4,7 +4,7 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'mongodb',
   connector: 'mongodb',
-  url: 'mongodb+srv://prog_web:minTIC2022@cluster0.ngw1n.mongodb.net/SolicitudesBD?retryWrites=true&w=majority',
+  url: '',
   host: '',
   port: 0,
   user: '',
